6b4a43ae549fd096985dbde6cce6504a76cfc555
Faradey27 <andrii.tiertyshnyi@gmail.com>
2018-11-10 00:34:57 +0200

    Add manifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,9 +14,19 @@ class MyDocument extends Document {
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <meta charSet="utf-8" />
           <meta name="description" content="Template for very fast React application"/>
+          <meta name="theme-color" content="#000000" />
+          <meta name="apple-mobile-web-app-title" content="Fast App Template" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <link rel="shortcut icon" href="/favicon.ico"></link>
+          <link rel="manifest" href="/manifest.json"></link>
           <style>{`body { margin: 0 }`}</style>
         </Head>
         <body>
+          <noscript>
+            You need to enable JavaScript to run this app.
+          </noscript>
           <Main />
           <NextScript />
         </body>
